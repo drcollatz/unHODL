4e8eb66386ec051e38a7c56e59a51853e778543a
Thilo Ackermann <thilo@ackermann.it>
2018-05-31 22:15:35 +0200

    my config

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var config = {};
 config.telegram = {};
 config.bitfinex = {};
 
-config.telegram.token = process.env.TELEGRAM_TOKEN || 'PUT TOKEN HERE';
-config.telegram.chat=  process.env.TELEGRAM_CHAT || 'PUT CHAT ID HERE';
+config.telegram.token = process.env.TELEGRAM_TOKEN || '606390384:AAFoP0gS3pWeKUA8Otph1cCRDDK-a1qx-ws';
+config.telegram.chat=  process.env.TELEGRAM_CHAT || '177582728';
 config.bitfinex.key = process.env.BITFINEX_KEY || 'PUT API KEY HERE';
 config.bitfinex.secret = process.env.BITFINEX_SECRET || 'PUT API SECRET HERE';
 
