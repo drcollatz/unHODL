5deeae5ac55985680d0d471b5781ad8945fe172c
tlowcode <thilo@ackermann.it>
2018-06-05 22:35:15 +0200

    Handled WebSocket promise rejection if already opened. Added amount calculation for short positions

--- Exchange.js
+++ Exchange.js
@@ -8,7 +8,6 @@ module.exports = class Exchange {
   constructor(inApiKey, inApiSecret) {
     if (!instance) {
       instance = this;
-      this.ws = [];
 
       this.bfx = new BFX({
         apiKey: inApiKey,
@@ -23,6 +22,10 @@ module.exports = class Exchange {
       this.rest = this.bfx.rest(2, {
         transform: true,
       });
+      this.ws = this.bfx.ws(2, {
+        manageCandles: true, // enable candle dataset persistence/management
+        transform: true, // converts ws data arrays to Candle models (and others)
+      });
     }
     return instance;
   }
@@ -34,26 +37,20 @@ module.exports = class Exchange {
      * @param {*} tradingPair
      */
   initWebSocket(tradingPair, onCandleCallback) {
-    const newWs = this.bfx.ws(2, {
-      manageCandles: true, // enable candle dataset persistence/management
-      transform: true, // converts ws data arrays to Candle models (and others)
-    });
-    if (newWs != null) {
-      this.ws.push(newWs);
-
-      newWs.on('error', (err) => {
+    if (this.ws != null) {
+      this.ws.on('error', (err) => {
         console.log(err);
       });
-      newWs.on('close', () => console.log('closed'));
+      this.ws.on('close', () => console.log('closed'));
 
-      newWs.on('open', () => {
-        newWs.auth.bind(newWs);
-        newWs.subscribeCandles(tradingPair.candleKey);
+      this.ws.on('open', () => {
+        this.ws.auth.bind(this.ws);
+        this.ws.subscribeCandles(tradingPair.candleKey);
         const msg = `Class ${this.constructor.name}: Websocket opened for ${tradingPair.candleKey}`;
         console.log(msg);
       });
 
-      newWs.onCandle({ key: tradingPair.candleKey }, (candles) => {
+      this.ws.onCandle({ key: tradingPair.candleKey }, (candles) => {
         const map = new Map();
         map.set('key', 'candleUpdate');
         map.set('context', this);
@@ -62,7 +59,9 @@ module.exports = class Exchange {
         // Database.savePriceToDb(this.currentPrice);
       });
 
-      newWs.open();
+      this.ws.open().catch(() => {
+        console.log(`Websocket ${this.ws.constructor.name} already opened -> Skipping...`);
+      });
     }
   }
 
