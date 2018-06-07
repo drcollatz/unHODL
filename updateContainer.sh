c9516dd8d9be6e5a1683b70f3050c3c1a8fe1e2c
Tobias Fink <tobifnk@gmail.com>
2018-06-07 20:51:04 +0200

    Fix update updaterContainer.sh script to work after move of config file.

--- updateContainer.sh
+++ updateContainer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+path=`pwd`
 docker stop unHODL
 docker container rm unHODL
 git pull
 docker build -t drcollatz/unhodl .
-docker run --name unHODL -d drcollatz/unhodl
+docker run -v $path/config:/app/conf --name unHODL -d drcollatz/unhodl
