00ec983a133ea318418ad04f11fa0633c9c6acff
Paul <paul@lewicki.name>
2018-06-08 00:54:26 +0200

    bugfix in docker script

--- updateContainer.sh
+++ updateContainer.sh
@@ -4,4 +4,4 @@ docker stop unHODL
 docker container rm unHODL
 git pull
 docker build -t drcollatz/unhodl .
-docker run -v $path/config:/app/conf --name unHODL -d drcollatz/unhodl
+docker run -v $path/conf:/app/conf --name unHODL -d drcollatz/unhodl
